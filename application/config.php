e011e1ab5a50aca4248cda096ae2f866be927bbb
pc_kang <king1963212100@gmail.com>
2017-09-25 02:09:47 +0800

    修改导航栏链接地址错误

--- application/config.php
+++ application/config.php
@@ -133,7 +133,7 @@ return [
     // 视图输出字符串内容替换
     'view_replace_str'       => [
         '__INDEX__'=> '/static/index',
-        '__MYURL__'=>'http://bick.boystark.com',
+        '__MYURL__'=>'http://bick.boystark.com/',
     ],
     // 默认跳转页面对应的模板文件
     'dispatch_success_tmpl'  => THINK_PATH . 'tpl' . DS . 'dispatch_jump.tpl',
